5234cdee305999bef35303f9bf0435464873339b
Valeriu Vancea <valeriuvancea@gmail.com>
2019-06-03 11:00:18 +0300

    Add heroku port

--- app.js
+++ app.js
@@ -8,4 +8,4 @@ companiesApi.set(app);
 var beneficialOwnersApi = require('./api/beneficialOwners');
 beneficialOwnersApi.set(app);
 
-app.listen(port, () => console.log(`App started and listenting on port ${port}`));
+app.listen(process.env.PORT || 80, () => console.log(`App started and listenting on port ${port}`));
