649d60aef7241628aa0c759b31d3c964d55db8c4
Valeriu Vancea <valeriuvancea@gmail.com>
2019-06-09 16:03:10 +0300

    Add access control headers to all requests

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000;
 app.use(express.json());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
