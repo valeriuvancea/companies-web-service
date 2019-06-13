5fd0c0cf7f9314d1008d17a2fe4f093ac208936f
Valeriu Vancea <valeriuvancea@gmail.com>
2019-06-14 00:23:49 +0300

    Add cors

--- app.js
+++ app.js
@@ -2,12 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.use(express.json());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods: POST, PUT, GET, OPTIONS")
-    next();
-});
+app.use(cors())
 
 var companiesApi = require('./api/companies');
 companiesApi.set(app);
